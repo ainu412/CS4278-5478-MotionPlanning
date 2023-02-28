714082586cdc4c3d98584f5315d3f9ae4037f45f
Guo,Ziyi <59839293+ainu412@users.noreply.github.com>
2023-02-28 09:52:27 +0800

    mock action plan Q1 derived class

--- src/planner/src/base_planner_Q1Q1.py
+++ src/planner/src/base_planner_Q1Q1.py
@@ -19,6 +19,8 @@ from planner.srv import (
     DiscreteActionStochasticExec,
 )
 
+from src.planner.src.base_planner1 import Planner, RobotClient
+
 ROBOT_SIZE = 0.2552  # width and height of robot in terms of stage unit
 
 
@@ -39,35 +41,8 @@ def dump_action_table(action_table, filename):
         json.dump(tab, fout)
 
 
-class Planner:
-    def __init__(self, world_width, world_height, world_resolution, inflation_ratio=3):
-        """init function of the base planner. You should develop your own planner
-        using this class as a base.
+class DSDAPlanner(Planner):
 
-        For standard mazes, width = 200, height = 200, resolution = 0.05. 
-        For COM1 map, width = 2500, height = 983, resolution = 0.02
-
-        Arguments:
-            world_width {int} -- width of map in terms of pixels
-            world_height {int} -- height of map in terms of pixels
-            world_resolution {float} -- resolution of map
-
-        Keyword Arguments:
-            inflation_ratio {int} -- [description] (default: {3})
-        """
-        self.map = None
-        self.pose = None
-        self.goal = None
-        self.action_seq = None  # output
-        self.aug_map = None  # occupancy grid with inflation
-        self.action_table = {}
-
-        self.world_width = world_width
-        self.world_height = world_height
-        self.resolution = world_resolution
-        self.inflation_ratio = inflation_ratio
-        self.setup_map()
-        rospy.sleep(1)
 
     def setup_map(self):
         """Get the occupancy grid and inflate the obstacle by some pixels.
@@ -124,26 +99,6 @@ class Planner:
 
     def xy_to_1d_grid_index(self, x, y):
         return y * self.world_width + x
-
-    def _get_goal_position(self):
-        goal_position = self.goal.pose.position
-        return (goal_position.x, goal_position.y)
-
-    def set_goal(self, x, y, theta=0):
-        """set the goal of the planner
-
-        Arguments:
-            x {int} -- x of the goal
-            y {int} -- y of the goal
-
-        Keyword Arguments:
-            theta {int} -- orientation of the goal; we don't consider it in our planner (default: {0})
-        """
-        a = PoseStamped()
-        a.pose.position.x = x
-        a.pose.position.y = y
-        a.pose.orientation.z = theta
-        self.goal = a
 
     def generate_plan(self, init_pose):
         """TODO: FILL ME! This function generates the plan for the robot, given
@@ -261,66 +216,66 @@ class Planner:
             cur_step += 1
 
 
-        #### for CSDA hybrid A*
-        # hybrid A* algorithm implementation, assuming continuous states
-        ## Euclidean distance as the heuristic H
-        def h_euclidean(x1, y1):
-            x2, y2 = self._get_goal_position()
-            return np.sqrt((x1 - x2)**2 + (y1 - y2)**2)
-        def pose_is_close_to_goal(x, y):
-            return math.sqrt( (x - self._get_goal_position()[0]) ** 2
-                              + (y - self._get_goal_position()[1]) ** 2 ) < 0.3
-
-        init_x, init_y, init_theta = init_pose
-        frontier = PriorityQueue()
-        frontier.put((0, (init_x, init_y, init_theta)))  # cost to come, priority queue: point location (x, y, theta)
-
-        g_score = {(init_x, init_y): 0}
-        f_score = {(init_x, init_y): h_euclidean(init_x, init_y)}
-        path_parent = dict()
-        path_control_from_parent = dict()
-
-        while frontier.not_empty:
-            current_f, (current_x, current_y, current_theta) = frontier.get()
-
-            if pose_is_close_to_goal(current_x, current_y):
-                path_seq_rev = [(current_x, current_y, current_theta)]
-                break
-
-            # sample a neighboring node that can be reached within one timestep
-            for _ in range(10):
-                # uniform sample v and w
-                v = np.random.uniform(0, 1)
-                w = np.random.uniform(-pi, pi)
-
-                nei_x, nei_y, nei_theta = self.motion_predict(cur_x, cur_y, current_theta, v, w)
-                # make sure next x, y is within boundary and occupancy rate is below 100
-                if not (0 <= nei_x < self.world_width
-                        and 0 <= nei_y < self.world_height):
-                    continue
-                if self.collision_checker(nei_x, nei_y) >= 100:
-                    continue
-
-                tmp_nei_g_score = g_score[(current_x, current_y)] + v / w * abs(nei_theta - current_theta)
-                tmp_nei_f_score = tmp_nei_g_score + h_euclidean(nei_x, nei_y)
-                nei_x_grid_index = int(nei_x)
-                nei_y_grid_index = int(nei_y)
-
-                if tmp_nei_f_score < f_score[(nei_x_grid_index, nei_y_grid_index)] \
-                        or (nei_x_grid_index, nei_y_grid_index) not in f_score:
-                    g_score[(nei_x_grid_index, nei_y_grid_index)] = tmp_nei_g_score
-                    f_score[(nei_x_grid_index, nei_y_grid_index)] = tmp_nei_f_score
-                    frontier.put((tmp_nei_f_score, (nei_x, nei_y, nei_theta)))
-                    path_parent[(nei_x, nei_y, nei_theta)] = (current_x, current_y, current_theta)
-                    path_control_from_parent[(nei_x, nei_y, nei_theta)] = (v, w)
-
-        # get action sequence according to sequence path tree
-        self.action_seq = []
-        while (current_x, current_y) != (init_x, init_y):
-            self.action_seq.append(path_control_from_parent[(current_x, current_y, current_theta)])
-            (current_x, current_y, current_theta) = path_parent[(current_x, current_y, current_theta)]
-
-        self.action_seq.reverse()
+        # #### for CSDA hybrid A*
+        # # hybrid A* algorithm implementation, assuming continuous states
+        # ## Euclidean distance as the heuristic H
+        # def h_euclidean(x1, y1):
+        #     x2, y2 = self._get_goal_position()
+        #     return np.sqrt((x1 - x2)**2 + (y1 - y2)**2)
+        # def pose_is_close_to_goal(x, y):
+        #     return math.sqrt( (x - self._get_goal_position()[0]) ** 2
+        #                       + (y - self._get_goal_position()[1]) ** 2 ) < 0.3
+        #
+        # init_x, init_y, init_theta = init_pose
+        # frontier = PriorityQueue()
+        # frontier.put((0, (init_x, init_y, init_theta)))  # cost to come, priority queue: point location (x, y, theta)
+        #
+        # g_score = {(init_x, init_y): 0}
+        # f_score = {(init_x, init_y): h_euclidean(init_x, init_y)}
+        # path_parent = dict()
+        # path_control_from_parent = dict()
+        #
+        # while frontier.not_empty:
+        #     current_f, (current_x, current_y, current_theta) = frontier.get()
+        #
+        #     if pose_is_close_to_goal(current_x, current_y):
+        #         path_seq_rev = [(current_x, current_y, current_theta)]
+        #         break
+        #
+        #     # sample a neighboring node that can be reached within one timestep
+        #     for _ in range(10):
+        #         # uniform sample v and w
+        #         v = np.random.uniform(0, 1)
+        #         w = np.random.uniform(-pi, pi)
+        #
+        #         nei_x, nei_y, nei_theta = self.motion_predict(cur_x, cur_y, current_theta, v, w)
+        #         # make sure next x, y is within boundary and occupancy rate is below 100
+        #         if not (0 <= nei_x < self.world_width
+        #                 and 0 <= nei_y < self.world_height):
+        #             continue
+        #         if self.collision_checker(nei_x, nei_y) >= 100:
+        #             continue
+        #
+        #         tmp_nei_g_score = g_score[(current_x, current_y)] + v / w * abs(nei_theta - current_theta)
+        #         tmp_nei_f_score = tmp_nei_g_score + h_euclidean(nei_x, nei_y)
+        #         nei_x_grid_index = int(nei_x)
+        #         nei_y_grid_index = int(nei_y)
+        #
+        #         if tmp_nei_f_score < f_score[(nei_x_grid_index, nei_y_grid_index)] \
+        #                 or (nei_x_grid_index, nei_y_grid_index) not in f_score:
+        #             g_score[(nei_x_grid_index, nei_y_grid_index)] = tmp_nei_g_score
+        #             f_score[(nei_x_grid_index, nei_y_grid_index)] = tmp_nei_f_score
+        #             frontier.put((tmp_nei_f_score, (nei_x, nei_y, nei_theta)))
+        #             path_parent[(nei_x, nei_y, nei_theta)] = (current_x, current_y, current_theta)
+        #             path_control_from_parent[(nei_x, nei_y, nei_theta)] = (v, w)
+        #
+        # # get action sequence according to sequence path tree
+        # self.action_seq = []
+        # while (current_x, current_y) != (init_x, init_y):
+        #     self.action_seq.append(path_control_from_parent[(current_x, current_y, current_theta)])
+        #     (current_x, current_y, current_theta) = path_parent[(current_x, current_y, current_theta)]
+        #
+        # self.action_seq.reverse()
 
 
         ### for DSPA
@@ -341,215 +296,8 @@ class Planner:
         Returns:
             bool -- True for collision, False for non-collision
         """
-        if (0 <= x < self.world_width / self.resolution and 0 <= y < self.world_height / self.resolution) \
-            and self.aug_map[self.xy_to_1d_grid_index(x, y)] == 100:
-            return True
-        return False
-
-    def motion_predict(self, x, y, theta, v, w, dt=0.5, frequency=10):
-        """Predict the next pose of the robot given controls. Returns None if
-        the robot collide with the wall.
-
-        The robot dynamics is provided in the assignment description.
-
-        Arguments:
-            x {float} -- current x of robot
-            y {float} -- current y of robot
-            theta {float} -- current theta of robot
-            v {float} -- linear velocity 
-            w {float} -- angular velocity
-
-        Keyword Arguments:
-            dt {float} -- time interval. DO NOT CHANGE (default: {0.5})
-            frequency {int} -- simulation frequency. DO NOT CHANGE (default: {10})
-
-        Returns:
-            tuple -- next x, y, theta; return None if has collision
-        """
-        num_steps = int(dt * frequency)
-        dx = 0
-        dy = 0
-        for i in range(num_steps):
-            if w != 0:
-                dx = - v / w * np.sin(theta) + v / w * \
-                     np.sin(theta + w / frequency)
-                dy = v / w * np.cos(theta) - v / w * \
-                     np.cos(theta + w / frequency)
-            else:
-                dx = v * np.cos(theta) / frequency
-                dy = v * np.sin(theta) / frequency
-            x += dx
-            y += dy
-
-            if self.collision_checker(x, y):
-                return None
-            theta += w / frequency
-        return x, y, theta
-
-    def discrete_motion_predict(self, x, y, theta, v, w, dt=0.5, frequency=10):
-        """Discrete version of the motion predict.
-
-        Note that since the ROS simulation interval is set to be 0.5 sec and the
-        robot has a limited angular speed, to achieve 90 degree turns, we have
-        to execute two discrete actions consecutively. This function wraps the
-        discrete motion predict.
-
-        Please use it for your discrete planner.
-
-        Arguments:
-            x {int} -- current x of robot
-            y {int} -- current y of robot
-            theta {int} -- current theta of robot
-            v {int} -- linear velocity
-            w {int} -- angular velocity (0, 1, 2, 3)
-
-        Keyword Arguments:
-            dt {float} -- time interval. DO NOT CHANGE (default: {0.5})
-            frequency {int} -- simulation frequency. DO NOT CHANGE (default: {10})
-
-        Returns:
-            tuple -- next x, y, theta; return None if has collision
-        """
-        w_radian = w * np.pi / 2
-        first_step = self.motion_predict(x, y, theta * np.pi / 2, v, w_radian)
-        if first_step:
-            second_step = self.motion_predict(
-                first_step[0], first_step[1], first_step[2], v, w_radian)
-            if second_step:
-                return (round(second_step[0]), round(second_step[1]), round(second_step[2] / (np.pi / 2)) % 4)
-        return None
-
-
-class RobotClient:
-    """A class to interface with the (simulated) robot.
-
-    You can think of this as the "driver" program provided by the robot manufacturer ;-)
-    """
-
-    def __init__(self):
-        self._cstate = None
-        self.sb_cstate = rospy.Subscriber(
-            "/lab1/continuous_state", ContinuousState, self._cstate_callback
-        )
-        self._dstate = None
-        self.sb_dstate = rospy.Subscriber(
-            "/lab1/discrete_state", DiscreteState, self._dstate_callback
-        )
-
-    def _cstate_callback(self, msg):
-        """Callback of the subscriber."""
-        self._cstate = msg
-
-    def get_current_continuous_state(self):
-        """Get the current continuous state.
-
-        Returns:
-            tuple -- x, y, \theta, as defined in the instruction document.
-        """
-        return (self._cstate.x, self._cstate.y, self._cstate.theta)
-
-    def _dstate_callback(self, msg):
-        self._dstate = msg
-
-    def get_current_discrete_state(self):
-        """Get the current discrete state.
-
-        Returns:
-            tuple -- x, y, \theta, as defined in the instruction document.
-        """
-        return (self._dstate.x, self._dstate.y, self._dstate.theta)
-
-    def _d_from_target(self, target_pose):
-        """Compute the distance from current pose to the target_pose.
-
-        Arguments:
-            pose {list} -- robot pose
-
-        Returns:
-            float -- distance to the target_pose
-        """
-        pose = self.get_current_continuous_state()
-        return math.sqrt(
-            (pose[0] - target_pose[0]) ** 2 + (pose[1] - target_pose[1]) ** 2
-        )
-
-    def is_close_to_goal(self, goal):
-        """Check if close enough to the given goal.
-
-        Arguments:
-            pose {list} -- robot post
-
-        Returns:
-            bool -- goal or not
-        """
-        return self._d_from_target(goal) < 0.3
-
-    def publish_discrete_control(self, action_seq, goal):
-        """Publish the discrete controls"""
-        proxy = rospy.ServiceProxy(
-            "/lab1/discrete_action_sequence",
-            DiscreteActionSequenceExec,
-        )
-        plan = [DiscreteAction(action) for action in action_seq]
-        proxy(plan)
-        assert self.is_close_to_goal(goal), "Didn't reach the goal."
-
-    def publish_continuous_control(self, action_seq, goal):
-        """Publish the continuous controls.
-
-        TODO: FILL ME!
-
-        You should implement the ROS service request to execute the motion plan.
-
-        The service name is /lab1/continuous_action_sequence
-
-        The service type is ContinuousActionSequenceExec
-
-        Checkout the definitions in planner/msg/ and planner/srv/
-        """
-        proxy = rospy.ServiceProxy(
-            "/lab1/continuous_action_sequence",
-            ContinuousActionSequenceExec,
-        )
-        plan = [ContinuousAction(action) for action in action_seq]
-        proxy(plan)
-
-        ############## <- end of FILL ME!
-
-        assert self.is_close_to_goal(goal)
-
-    def execute_policy(self, action_table, goal):
-        """Execute a given policy in MDP.
-
-        Due to the stochastic dynamics, we cannot execute the motion plan
-        without feedback. Hence, every time we execute a discrete action, we
-        query the current state by `get_current_discrete_state()`.
-
-        You don't have to worry about the stochastic dynamics; it is implemented
-        in the simulator. You only need to send the discrete action.
-        """
-        # TODO: FILL ME!
-        # Instantiate the ROS service client
-        # Service name: /lab1/discrete_action_stochastic
-        # Service type: DiscreteActionStochasticExec
-        # Checkout the definitions in planner/msg/ and planner/srv/
-        proxy = rospy.ServiceProxy(
-            "/lab1/discrete_action_stochastic",
-            DiscreteActionStochasticExec,
-        )
-        #####################<- end of FILL ME!
-
-
-        while not self.is_close_to_goal(goal):
-            current_state = self.get_current_discrete_state()
-            action = action_table[current_state]
-            # TODO: FILL ME!
-            # Put the action into proper ROS request and send it
-            proxy(action)
-            #####################<- end of FILL ME!
-
-        rospy.sleep(1)
-        assert self.is_close_to_goal(goal)
+        return (0 <= x < self.world_width / self.resolution and 0 <= y < self.world_height / self.resolution) \
+            and self.aug_map[self.xy_to_1d_grid_index(x, y)] == 100
 
 
 if __name__ == "__main__":
@@ -603,7 +351,7 @@ if __name__ == "__main__":
     # Let's try executing a hard-coded motion plan!
     # Forward! Forward! Turn left!
     # You should see an AssertionError since we didn't reach the goal.
-    mock_action_plan = [(1, 0), (1, 0), (0, 1)]
+    mock_action_plan = [(1, 0), (1, 0), (0, 1), (0, -1), (1, 0), (1, 0)]
     robot.publish_discrete_control(mock_action_plan, goal)
     # TODO: FILL ME!
     # After you implement your planner, send the plan/policy generated by your
