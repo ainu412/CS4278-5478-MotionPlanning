dd40bf928fa69a5b8ee38cb7cb57818719c7b600
Guo,Ziyi <59839293+ainu412@users.noreply.github.com>
2023-02-28 11:44:54 +0800

    Update base_planner1.py

--- src/planner/src/base_planner1.py
+++ src/planner/src/base_planner1.py
@@ -416,7 +416,7 @@ class DSDAPlanner(Planner):
         self.aug_map = tuple(self.aug_map)
 
         for y in range(199, -1 , -1):
-            print(['+' if self.aug_map[self.xy_to_1d_grid_index(x, y)] == 100 else ' ' for x in range(200)])
+            print("".join(['+' if self.aug_map[self.xy_to_1d_grid_index(x, y)] == 100 else ' ' for x in range(200)]))
             # print(i, "augmap {200*i} to {200*(i+1)}", self.aug_map[200*i:200*i+200])
         ###################################<- end of FILL ME
 
