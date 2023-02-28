d6718948ad4f9158a4a6e2ca60889f29e4caf708
Guo,Ziyi <59839293+ainu412@users.noreply.github.com>
2023-02-28 11:38:37 +0800

    Update base_planner1.py

--- src/planner/src/base_planner1.py
+++ src/planner/src/base_planner1.py
@@ -417,7 +417,7 @@ class DSDAPlanner(Planner):
 
         for x in range(200):
             for y in range(200, 0 , -1):
-                print('+' if self.aug_map[self.xy_to_1d_grid_index(x. y)] == 100 else ' ')
+                print('+' if self.aug_map[self.xy_to_1d_grid_index(x, y)] == 100 else ' ')
             # print(i, "augmap {200*i} to {200*(i+1)}", self.aug_map[200*i:200*i+200])
         ###################################<- end of FILL ME
 
