39f6b19558e172e0a2279076abb149b78d7508e9
Guo,Ziyi <59839293+ainu412@users.noreply.github.com>
2023-02-28 23:00:25 +0800

    check if path has problem or path to action has problem

--- src/planner/src/direct_below_one.py
+++ src/planner/src/direct_below_one.py
@@ -671,6 +671,7 @@ class CSDAPlanner(Planner):
 
         print('path_parent', path_parent)
         print('path_control_from_parent', path_control_from_parent)
+        
         # get action sequence according to sequence path tree
         self.action_seq = []
         while (current_x, current_y) != (init_x, init_y):
