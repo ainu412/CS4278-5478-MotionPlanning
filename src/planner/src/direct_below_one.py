306f2cd0867c991aa668dc84946749cccd5718ef
Guo,Ziyi <59839293+ainu412@users.noreply.github.com>
2023-03-01 07:16:29 +0800

    try resolve motion_predict noneType problem

--- src/planner/src/direct_below_one.py
+++ src/planner/src/direct_below_one.py
@@ -370,7 +370,45 @@ class CSDAPlanner(Planner):
         def loc_to_grid_index(x, y):
             return int(x / grid_resolution), int(y / grid_resolution)
 
+        def motion_predict(self, x, y, theta, v, w, dt=0.5, frequency=10):
+            """Predict the next pose of the robot given controls. Returns None if
+            the robot collide with the wall.
 
+            The robot dynamics is provided in the assignment description.
+
+            Arguments:
+                x {float} -- current x of robot
+                y {float} -- current y of robot
+                theta {float} -- current theta of robot
+                v {float} -- linear velocity
+                w {float} -- angular velocity
+
+            Keyword Arguments:
+                dt {float} -- time interval. DO NOT CHANGE (default: {0.5})
+                frequency {int} -- simulation frequency. DO NOT CHANGE (default: {10})
+
+            Returns:
+                tuple -- next x, y, theta; return None if has collision
+            """
+            num_steps = int(dt * frequency)
+            dx = 0
+            dy = 0
+            for i in range(num_steps):
+                if w != 0:
+                    dx = - v / w * np.sin(theta) + v / w * \
+                         np.sin(theta + w / frequency)
+                    dy = v / w * np.cos(theta) - v / w * \
+                         np.cos(theta + w / frequency)
+                else:
+                    dx = v * np.cos(theta) / frequency
+                    dy = v * np.sin(theta) / frequency
+                x += dx
+                y += dy
+
+                if self.collision_checker(x, y):
+                    return None
+                theta += w / frequency
+            return x, y, theta
 
         init_x, init_y, init_theta = init_pose
         g_score = {loc_to_grid_index(init_x, init_y): 0}
